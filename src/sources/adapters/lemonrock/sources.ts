7a098d7d68faf7d8b0d4e49027cd4c540b487b29
Flowency <jason@flowency.co.uk>
2026-08-24 21:19:31 +0100

    Use live Lemonrock national index roots

--- src/sources/adapters/lemonrock/sources.ts
+++ src/sources/adapters/lemonrock/sources.ts
@@ -28,11 +28,11 @@ const common: Pick<GigSource,
 export const LEMONROCK_SOURCES: GigSource[] = [
   { ...common, id: 'lemonrock-new-gigs', name: 'Lemonrock newly posted gigs', url: 'https://www.lemonrock.com/newestgigs.php' },
   { ...common, id: 'lemonrock-cancellations', name: 'Lemonrock explicit cancellations', url: 'https://www.lemonrock.com/cancellations.php' },
-  { ...common, id: 'lemonrock-artist-index', name: 'Lemonrock national artist index', url: 'https://www.lemonrock.com/advancedsearchbands.php?_start=0' },
+  { ...common, id: 'lemonrock-artist-index', name: 'Lemonrock national artist index', url: 'https://www.lemonrock.com/allbands.php' },
   { ...common, id: 'lemonrock-venue-index', name: 'Lemonrock national venue index', url: 'https://www.lemonrock.com/allvenues.php' },
   { ...common, id: 'lemonrock-artist-hydration', name: 'Lemonrock artist profiles' },
   { ...common, id: 'lemonrock-venue-hydration', name: 'Lemonrock venue profiles' },
   { ...common, id: 'lemonrock-gig-hydration', name: 'Lemonrock gig details' },
-  { ...common, id: 'lemonrock-future-reconcile', name: 'Lemonrock national future-gig reconcile', url: 'https://www.lemonrock.com/' },
+  { ...common, id: 'lemonrock-future-reconcile', name: 'Lemonrock national future-gig reconcile', url: 'https://www.lemonrock.com/gigsbycounty.php' },
   { ...common, id: 'lemonrock-full-reconcile', name: 'Lemonrock full reconciliation', url: 'https://www.lemonrock.com/' },
 ];
